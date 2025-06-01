72317df1dff446082929db37e8e8d9e48737be32
vydray <vydray@vy-dray.com>
2025-06-01 20:24:59 +0900

    詳細モーダルから席移動ボタンを削除（長押し移動は維持）

--- pages/index.tsx
+++ pages/index.tsx
@@ -613,16 +613,9 @@ export default function Home() {
               </div>
               
               <div className="button-group">
-                <button onClick={checkout} className="btn-warning">会計完了</button>
-                <button onClick={() => {
-                  setShowModal(false)
-                  setShowMoveModal(true)
-                }} className="btn-primary">席移動</button>
-                <button onClick={clearTable} className="btn-danger">削除</button>
-              </div>
-            </div>
-          )}
-        </div>
+  <button onClick={checkout} className="btn-warning">会計完了</button>
+  <button onClick={clearTable} className="btn-danger">削除</button>
+</div>
       )}
 
       {/* 席移動モーダル */}
