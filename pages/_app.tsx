1961a3ab21bc2f2bacafe29e8a0bae2f6ec41e04
vydray <vydray@vy-dray.com>
2025-06-02 22:00:55 +0900

    ログイン機能実装とレイアウト修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css' // グローバルスタイルをインポート（もしあれば）
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
