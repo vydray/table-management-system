c305ef8daed3ef684b2016ca3e4a10023c47b47d
vydray <vydray@vy-dray.com>
2025-07-15 01:44:55 +0900

    fix: プラグイン登録方法を修正

--- android/app/src/main/java/com/vydray/pos/MainActivity.java
+++ android/app/src/main/java/com/vydray/pos/MainActivity.java
@@ -2,13 +2,20 @@ package com.vydray.pos;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.PluginManager;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // SiiPrinterプラグインを登録
-        registerPlugin(SiiPrinterPlugin.class);
+    }
+    
+    @Override
+    protected void init(Bundle savedInstanceState, ArrayList<Class<? extends Plugin>> plugins) {
+        // SiiPrinterプラグインを追加
+        plugins.add(SiiPrinterPlugin.class);
+        super.init(savedInstanceState, plugins);
     }
 }
